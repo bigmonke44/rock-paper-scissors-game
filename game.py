1e30006fc73256bf19c09a3168d21deccf3fd9e4
bigmonke44 <114050950+bigmonke44@users.noreply.github.com>
2022-09-28 08:19:46 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,13 +1,21 @@
-human_turn = input('Input human turn: ') 
-computer_turn = input('Input computer turn: ')
+import random
+turns = ["rock", "paper", "scissors" ]
 
-if human_turn == computer_turn :
-    print('Draw!')
-elif human_turn == 'rock' and computer_turn == 'scissors' :
-    print('Human_wins!')
-elif human_turn == 'paper' and computer_turn == 'rock' :
-    print('Human_wins!')
-elif human_turn == 'scissors' and computer_turn == 'paper' :
-    print('Human_wins!')
-else:
-    print('Computer_wins!')
+
+while(True):
+    human_turn = input("enter your turn,or type exit: ")
+    computer_turn = random.choice(turns)
+
+    if human_turn == 'exit':
+        break
+    print(f'Human:{human_turn} vs. {computer_turn}')
+    if human_turn == computer_turn:
+        print("Draw!")
+    elif human_turn == "rock" and computer_turn == "scissors":
+        print("Human wins!")
+    elif human_turn == "paper" and computer_turn == "rock":
+        print("Human wins!")
+    elif human_turn == "scissors" and computer_turn == "paper":
+        print("Human wins!")
+    else:
+        print("Computer wins!")
